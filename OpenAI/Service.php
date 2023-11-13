81d5aeb39578c352972aa1f6bc95c9bff2b465de
abramovku <abramovku@gmail.com>
2023-11-13 21:45:25 +0200

    init

--- OpenAI/Service.php
+++ OpenAI/Service.php
@@ -41,23 +41,44 @@ class Service
         return html_entity_decode($text);
     }
 
-    private function preparePrompt(string $prompt, ?string $prefix = null ): array
-    {
-        $prompt = $this->sanitizeText($prompt);
-        if ($prefix === null) {
-            $prefix = "Подготовь мне описание на русском языке для товара в интернет магазине: ";
-        }
+	private function preparePrompt(string $prompt, ?string $prefix = null, ?string $suffix = null): array
+	{
+		$prompt = $this->sanitizeText($prompt);
+		if ($prefix === null) {
+			$prefix = "Подготовь мне описание на русском языке для товара в интернет магазине: ";
+		}
 
-        return [
-            "model" => "gpt-3.5-turbo",
-            "messages" => [
-                [
-                    "role" => "user",
-                    "content" => $prefix . $prompt
-                ]
-            ]
-        ];
-    }
+		if ($suffix === null) {
+			$suffix = "";
+		}
 
+		return [
+			"model" => "gpt-3.5-turbo",
+			"messages" => [
+				[
+					"role" => "user",
+					"content" => $prefix . $prompt . $suffix
+				]
+			]
+		];
+	}
 
+	public function sendPromptWithPrefix(string $prompt, string $prefix, string $suffix): string
+	{
+		$result = $this->client->post('chat/completions', $this->preparePrompt($prompt, $prefix, $suffix));
+
+		if (empty($result)) {
+			throw new Exception('Wrong response from OpenAI');
+		}
+
+		if (!empty($result['error'])) {
+			throw new Exception($result['error']['message'] . $result['error']['code']);
+		}
+
+		if (empty($result['choices'][0]['message']['content'])) {
+			throw new Exception('Empty response from OpenAI');
+		}
+
+		return $this->sanitizeText($result['choices'][0]['message']['content']);
+	}
 }
